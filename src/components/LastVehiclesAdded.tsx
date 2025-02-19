098d5e1708fa5fceff94666771210dbe22288e47
Gamberge <cedric.righi@gmail.com>
2025-02-19 11:59:12 +0100

    fix responsive

--- src/components/LastVehiclesAdded.tsx
+++ src/components/LastVehiclesAdded.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import "../styles/LastVehiclesAdded.css";
 import VehiclesCard from "./OneVehicleCard";
 
@@ -16,25 +16,7 @@ interface VehiclesProps {
 }
 
 export default function LastVehiclesAdded() {
-	const [latestVehiclesList, setLatestVehiclesList] = useState<VehiclesProps[]>(
-		[],
-	);
-
-	const getLastVehiclesAdded = useCallback(async () => {
-		try {
-			const response = await fetch(
-				`${import.meta.env.VITE_API_URL}/api/latest-vehicles`,
-			);
-			const data = await response.json();
-			setLatestVehiclesList(data);
-		} catch (error) {
-			console.error(error);
-		}
-	}, []);
-
-	useEffect(() => {
-		getLastVehiclesAdded();
-	}, [getLastVehiclesAdded]);
+	const [latestVehiclesList] = useState<VehiclesProps[]>([]);
 
 	return (
 		<section className="latest-vehicles-added-container">
