25d690ee8608d4d2fe95c29a60609395aa88eebd
Gamberge <cedric.righi@gmail.com>
2025-02-24 15:05:39 +0100

    fix: fix latest vehicles added list

--- src/components/LastVehiclesAdded.tsx
+++ src/components/LastVehiclesAdded.tsx
@@ -17,7 +17,49 @@ interface VehiclesProps {
 }
 
 export default function LastVehiclesAdded() {
-	const [latestVehiclesList] = useState<VehiclesProps[]>([]);
+	const vehiclesArray = [
+		{
+			id: 5,
+			category_id: 4,
+			image: "/images/hyundai-i10.jpg",
+			brand: "Hyundai",
+			model: "i10",
+			year: 2021,
+			mileage: 5000,
+			consumption: 5,
+			transmission: "manuelle",
+			price: 5000,
+			user_id: 1,
+		},
+		{
+			id: 6,
+			category_id: 2,
+			image: "/images/lambo-sto.jpg",
+			brand: "Lamborghini",
+			model: "Huracan STO",
+			year: 2021,
+			mileage: 5000,
+			consumption: 20,
+			transmission: "automatique",
+			price: 300000,
+			user_id: 4,
+		},
+		{
+			id: 7,
+			category_id: 2,
+			image: "/images/nissan-gtr35.jpg",
+			brand: "Nissan",
+			model: "GTR35",
+			year: 2015,
+			mileage: 150000,
+			consumption: 15,
+			transmission: "automatique",
+			price: 60000,
+			user_id: 3,
+		},
+	];
+
+	const [latestVehiclesList] = useState<VehiclesProps[]>(vehiclesArray);
 
 	return (
 		<section className="latest-vehicles-added-container">
